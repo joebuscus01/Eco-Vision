40b95f7f4562c30a0b37757140adf1e466e50f54
joebuscus <158345506+joebuscus01@users.noreply.github.com>
2025-03-20 22:28:46 -0400

    Add files via upload

--- Eco vision/js/login.js
+++ Eco vision/js/login.js
@@ -1,3 +1,14 @@
 function Resgister(){
     window.location.href = 'Register.html';
 }
+
+function login_valid(){
+    return true;
+}
+
+function two_fa(){
+        const log_window = document.querySelector(".login-wrapper")
+        log_window.style.display = "none"
+        const two_fa = document.querySelector(".two-fa")
+        two_fa.style.display = "flex"
+}
