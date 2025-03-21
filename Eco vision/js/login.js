78c4b3f0e8c015e026d012844f401515af3f2bcf
joebuscus <158345506+joebuscus01@users.noreply.github.com>
2025-03-21 16:05:39 -0400

    Add files via upload

--- Eco vision/js/login.js
+++ Eco vision/js/login.js
@@ -1,3 +1,6 @@
+
+
+
 function Resgister(){
     window.location.href = 'Register.html';
 }
@@ -7,8 +10,18 @@ function login_valid(){
 }
 
 function two_fa(){
-        const log_window = document.querySelector(".login-wrapper")
+    const log_window = document.querySelector(".login-wrapper") // see if u can make this global
+
         log_window.style.display = "none"
-        const two_fa = document.querySelector(".two-fa")
-        two_fa.style.display = "flex"
+    const two_fa = document.getElementById("two-fa")
+    two_fa.style.display = "flex"
+        
+}
+
+function two_fa_return(){
+    const log_window = document.querySelector(".login-wrapper") // see if u can make this global
+        log_window.style.display = "flex"
+    const two_fa = document.getElementById("two-fa")
+    two_fa.style.display = "none"
+
 }
