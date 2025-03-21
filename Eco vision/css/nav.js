b111379e0fe47010d8ebfc169bb63b707408b600
joebuscus <158345506+joebuscus01@users.noreply.github.com>
2025-03-20 22:29:14 -0400

    Add files via upload

---
+++ Eco vision/css/nav.js
@@ -0,0 +1,10 @@
+function ShowSidebar(){
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "flex"
+
+} 
+function HideSidebar(){
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "none"
+
+}
