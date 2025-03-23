c17ee605c63e754825f441871e47d4602f0ad72e
joebuscus <158345506+joebuscus01@users.noreply.github.com>
2025-03-23 13:09:51 -0400

    Add files via upload

--- Eco vision/js/contact.js
+++ Eco vision/js/contact.js
@@ -1,5 +1,6 @@
+let form = document.querySelector(".contact-form"); // Selects the form using its class
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-})
-
+    console.log("Form submission prevented!");
+});
